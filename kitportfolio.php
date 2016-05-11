6a36f317dc821ffbe965dedfbfa468f170e6b8b3
Mark A Jones <mark@bmkdigital.co.uk>
2016-05-11 19:16:19 +0100

    Moved Light Box Trigger to includes File

--- kitportfolio.php
+++ kitportfolio.php
@@ -79,26 +79,7 @@ class Kitportfolio
 
 
         }
-
-
-        if ($options['enablelightbox'] == 'yes') {
-            if ($options['lightboxmode'] == 'fancybox') {
-                wp_enqueue_script('fancybox',
-                    get_site_url() . '/wp-content/plugins/kodeportfolio/fancybox/jquery.fancybox-1.3.4.pack.js',
-                    array('jquery'));
-                wp_enqueue_style('fancycss',
-                    get_site_url() . '/wp-content/plugins/kodeportfolio/fancybox/jquery.fancybox-1.3.4.css');
-
-                wp_enqueue_script(
-                    'gofancybox',
-                    get_site_url() . '/wp-content/plugins/kodeportfolio/fancybox/gofancy.js',
-                    array('fancybox')
-                );
-
-            }
-        }
-
-
+        include_once ('/wp-content/plugins/kitportfolio/includes/index_lightboxOptions.php');
     }
 
 
