c92e4f67fbd24dd47580b5b58223d736ee83cb6f
Mark A Jones <mark@bmkdigital.co.uk>
2016-05-11 19:21:28 +0100

    well Just say it was a bad idear

--- kitportfolio.php
+++ kitportfolio.php
@@ -79,7 +79,26 @@ class Kitportfolio
 
 
         }
-        include_once ('/wp-content/plugins/kitportfolio/includes/index_lightboxOptions.php');
+
+
+        if ($options['enablelightbox'] == 'yes') {
+            if ($options['lightboxmode'] == 'fancybox') {
+                wp_enqueue_script('fancybox',
+                    get_site_url() . '/wp-content/plugins/kodeportfolio/fancybox/jquery.fancybox-1.3.4.pack.js',
+                    array('jquery'));
+                wp_enqueue_style('fancycss',
+                    get_site_url() . '/wp-content/plugins/kodeportfolio/fancybox/jquery.fancybox-1.3.4.css');
+
+                wp_enqueue_script(
+                    'gofancybox',
+                    get_site_url() . '/wp-content/plugins/kodeportfolio/fancybox/gofancy.js',
+                    array('fancybox')
+                );
+
+            }
+        }
+
+
     }
 
 
